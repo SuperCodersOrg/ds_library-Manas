1751a4440f1b6b745f94fe11f943681a4f08744a
manaschawla <manaschawla324@gmail.com>
2026-06-25 10:32:36 +0530

    Add HashMap remove,copy and rehashing

--- src/Hash Map/hash_map.h
+++ src/Hash Map/hash_map.h
@@ -50,6 +50,78 @@ public:
         return ((int)key) % bucketCount;
     }
 };
+
+template<>
+class DefaultHasher<long>
+{
+public:
+
+    int hash(
+        const long& key,
+        int bucketCount
+    ) const
+    {
+        return key % bucketCount;
+    }
+};
+
+template<>
+class DefaultHasher<long long>
+{
+public:
+
+    int hash(
+        const long long& key,
+        int bucketCount
+    ) const
+    {
+        return key % bucketCount;
+    }
+};
+
+template<>
+class DefaultHasher<float>
+{
+public:
+
+    int hash(
+        const float& key,
+        int bucketCount
+    ) const
+    {
+        return ((int)(key * 1000))
+               % bucketCount;
+    }
+};
+template<>
+class DefaultHasher<bool>
+{
+public:
+
+    int hash(
+        const bool& key,
+        int bucketCount
+    ) const
+    {
+        return ((int)key)
+               % bucketCount;
+    }
+};
+template<>
+class DefaultHasher<double>
+{
+public:
+
+    int hash(
+        const double& key,
+        int bucketCount
+    ) const
+    {
+        return ((long long)(key * 1000000))
+               % bucketCount;
+    }
+};
+
 template<>
 class DefaultHasher<std::string>
 {
@@ -76,12 +148,12 @@ private:
     int currentSize;
     int bucketCount;
     DefaultHasher<K> hasher;
+    void rehash();
 
 public:
     HashMap();
     HashMap(const HashMap& other);
     HashMap& operator=(const HashMap& other);
-    ~HashMap();
     void put(const K& key,const V& value);
     V get(const K& key) const;
     bool containsKey(const K& key) const;
@@ -106,9 +178,54 @@ HashMap<K,V>::HashMap()
 }
 
 template<typename K, typename V>
+HashMap<K,V>::HashMap(const HashMap& other)
+{
+    buckets = other.buckets;
+    currentSize = other.currentSize;
+    bucketCount = other.bucketCount;
+}
+
+template<typename K, typename V>
+HashMap<K,V>& HashMap<K,V>::operator=(const HashMap& other)
+{
+    if(this == &other)
+    {
+        return *this;
+    }
+    buckets = other.buckets;
+    currentSize = other.currentSize;
+    bucketCount = other.bucketCount;
+    return *this;
+}
+
+template<typename K, typename V>
 int HashMap<K,V>::size() const
 {
     return currentSize;
+}
+
+template<typename K, typename V>
+void HashMap<K,V>::rehash()
+{
+    DynamicArray<LinkedList<Entry<K,V>>> oldBuckets = buckets;
+    bucketCount = bucketCount * 2;
+    buckets.clear();
+    for(int i = 0; i < bucketCount; i++)
+    {
+        buckets.append(LinkedList<Entry<K,V>>());
+    }
+    int oldSize = currentSize;
+    currentSize = 0;
+    for(int i = 0; i < oldBuckets.size(); i++)
+    {
+        LinkedList<Entry<K,V>>& bucket = oldBuckets.get(i);
+        for(int j = 0; j < bucket.size(); j++)
+        {
+            Entry<K,V> entry = bucket.get(j);
+            put(entry.key,entry.value);
+        }
+    }
+    currentSize = oldSize;
 }
 
 template<typename K, typename V>
@@ -153,8 +270,11 @@ V HashMap<K,V>::get(const K& key) const
 template<typename K, typename V>
 void HashMap<K,V>::put(const K& key,const V& value)
 {
+    if(currentSize * 4 >= bucketCount * 3)
+    {
+        rehash();
+    }
     int index = hasher.hash(key,bucketCount);
-
     LinkedList<Entry<K,V>>& bucket = buckets.get(index);
     for(int i = 0; i < bucket.size(); i++)
     {
@@ -168,3 +288,29 @@ void HashMap<K,V>::put(const K& key,const V& value)
     currentSize++;
 }
 
+template<typename K, typename V>
+void HashMap<K,V>::remove(const K& key)
+{
+    int index =hasher.hash(key,bucketCount);
+    LinkedList<Entry<K,V>>& bucket = buckets.get(index);
+    for(int i = 0; i < bucket.size(); i++)
+    {
+        if(bucket.get(i).key == key)
+        {
+            bucket.remove(i);
+            currentSize--;
+            return;
+        }
+    }
+    throw std::out_of_range("Key not found");
+}
+
+template<typename K, typename V>
+void HashMap<K,V>::clear()
+{
+    for(int i = 0; i < bucketCount; i++)
+    {
+        buckets.get(i).clear();
+    }
+    currentSize = 0;
+}
